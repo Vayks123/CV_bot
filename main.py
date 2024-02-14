cbded2510822884b96b0338461a50245e365d938
Vayks123 <127139772+Vayks123@users.noreply.github.com>
2024-02-14 12:10:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ dp.include_routers(user_router)
 
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
