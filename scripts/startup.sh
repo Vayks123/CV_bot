1c9e642ade0925831d2a585350f0756ad2a2fb4f
Serhii Bibik <sergybibik1234@gmail.com>
2024-03-07 12:27:29 +0200

    Update aws deploy

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,6 +3,13 @@ set -x
 set -e
 
 python3 --version
-pip --version
+
+if pip --version ; then
+    echo "pip is installed"
+else
+    sudo apt update -y && sudo apt install python3-pip
+fi
+
+pip install -r requirment.txt
 killall python3
 python main.py
