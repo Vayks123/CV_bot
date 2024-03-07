e3eb5261676eefe0b433c3974e467845ce740a69
Serhii Bibik <sergybibik1234@gmail.com>
2024-03-07 12:49:51 +0200

    change a scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,9 +7,10 @@ python3 --version
 if pip --version ; then
     echo "pip is installed"
 else
+    echo "pip is uninstalled"
     sudo apt update -y && sudo apt install python3-pip
 fi
 
-pip install -r requirments.txt
+python3 -m pip install -r requirments.txt
 killall python3
 python main.py
