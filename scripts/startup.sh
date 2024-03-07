bb14b35965a5177f70118012b0df642ee50399a0
Serhii Bibik <sergybibik1234@gmail.com>
2024-03-07 15:33:20 +0200

    add systemd unit

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,12 +1,16 @@
 #!/bin/env bash
 
 sudo apt install -y python3-venv
+sudo cp /tmp/telegrambot.service /etc/systemd/system/
 
 python3 --version
 
 python3 -m venv venv
 source venv/bin/activate 
 pip3 install -r requirements.txt
-# killall python3 
-which python3
-# python3 main.py 
+
+sudo sudo systemctl daemon-reload
+sudo systemctl start your_unit
+sudo systemctl enable your_unit
+sudo systemctl status your_unit
+
