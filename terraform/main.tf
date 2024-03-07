a0b48b1c33e6e9e95f3129f892f257430a97abdc
Serhii Bibik <sergybibik1234@gmail.com>
2024-03-07 16:27:19 +0200

    add a s3 backend for tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  
+  backend "s3" {
+    bucket = "terraform-state-cv-bot"
+    key    = "terraform.tfstate"
+    encrypt = true
+    region = "eu-central-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
